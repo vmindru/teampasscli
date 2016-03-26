f862769b305ae3ede538009dea17130e4214d513
Veaceslav Mindru <mindruv@gmail.com>
2016-03-26 11:05:48 +0100

    init, wip

--- cli.py
+++ cli.py
@@ -2,13 +2,14 @@
 
 from optparse import OptionParser
 from sys import exit
+import StringIO
 import os
 import pycurl
 
 progvers = 0.1
 
 KEY = os.environ['TIM_KEY']
-URL = os.environ['TIM_URL']
+url = os.environ['TIM_URL']
 
 
 class BuildOpts:
@@ -24,10 +25,10 @@ class BuildOpts:
                                    )
         self.parser.description = ('TeamPass,search passwords with CLI')
         self.parser.add_option("-r",
-                               "--request",
-                               dest="request",
+                               "--action",
+                               dest="action",
                                default='find',
-                               help="specify the request e.g. find")
+                               help="specify the action e.g. find")
         self.parser.add_option("-i",
                                "--item",
                                dest="item",
@@ -39,29 +40,46 @@ class BuildOpts:
         return options
 
     def validate_options(self):
-        valid_request = ["find", "get"]
-        if not self.options.request in valid_request:
-            print self.options.request + " is not a valid request"
+        valid_action = ["find", "get"]
+        if not self.options.action in valid_action:
+            print self.options.action + " is not a valid action"
             self.parser.print_help()
             exit(1)
 
 
-class BUILD_URL:
-    def __init__(self, opts):
-        self.REQUEST_URL = ''
-        self.REQUEST = opts.request
+class URL:
+    def __init__(self, options):
+        self.request_url = ''
+        self.request = options.request
         self.build_url()
+    def build_request(self):
+        request = '/'+self.options.action+'/'+'/'+'vmindru'+self.options.item
 
-    def build_url(self, REQUEST):
-        self.REQUEST_URL = (os.environ['TIM_URL'] +
-                    '/api/index.php' +
-                    REQUEST+'?apikey=' +
-                    KEY)
+    def build_url(self):
+        self.request_url = (os.environ['TIM_URL'] +
+                            '/api/index.php' +
+                            self.request+'?apikey=' +
+                            KEY)
+        return self.request_url
 
 
-b = BuildOpts()
-options = b.options
+class MAIN:
+    def __init__(self):
+        mOpts = BuildOpts()
+        self.options = mOpts.options
+        self.url = URL(self.options)
 
-c = pycurl.Curl()
-#c.setopt(pycurl.URL, URL)
-#c.perform()
+    def curl_request(self, url, options):
+        c = pycurl.Curl()
+        b = StringIO.StringIO()
+        c.setopt(pycurl.URL, url)
+        c.setopt(pycurl.WRITEFUNCTION, b.write)
+        c.setopt(pycurl.FOLLOWLOCATION, 1)
+        c.setopt(pycurl.MAXREDIRS, 5)
+        c.perform()
+        html = b.getvalue()
+        return html
+
+if __name__ == "__main__":
+    m = MAIN()
+    print m.curl_request()
